4db228cf28685ea32caacaf9e4620efbfd543595
Kevin <marchinkev@gmail.com>
2017-11-20 23:19:47 +0000

    Fixed amd

--- src/amd.js
+++ src/amd.js
@@ -1,16 +1,7 @@
-var fs = require('fs');
+const {promisify} = require('util');
 
-function backgroundReadFile(file) {
-  return new Promise(function(suceed, fail) {
-    fs.readFile(file, function(err, data) {
-  		if (err) {
-  			fail(err);
-  		} else {
-        suceed(data);
-      }
-    });
-  });
-}
+var fs = require('fs');
+var backgroundReadFile = promisify(fs.readFile);
 
 var defineCache = Object.create(null);
 var currentMod = {
@@ -20,24 +11,25 @@ var currentMod = {
 };
 
 function getModule(name) {
-  return new Promise(function(suceed, fail) {
+  return new Promise((success, fail) => {
     if (name in defineCache)
-      suceed(defineCache[name]);
+      success(defineCache[name]);
 
     var module = {
       exports: null,
-  		loaded: false,
-  		onLoad: []
+      loaded: false,
+      onLoad: []
     };
 
-    backgroundReadFile(name + ".js").then(function(code) {
-      new Function("", code)();
+    backgroundReadFile(name + ".js").then((code) => {
 
       currentMod = module;
+      new Function("", code)();
+
       defineCache[name] = module;
       success(module);
 
-    }, function(error) {
+    }, (error) => {
       fail(new Error(error));
     });
   });
@@ -47,28 +39,30 @@ module.exports = function (depNames, moduleFunction) {
   var myMod = currentMod;
   var deps = depNames.map(getModule);
 
-  deps.forEach(function(promises) {
-    promises.then(function(mod) {
-      if (!mod.loaded)
+  Promise.all(deps).then((modules) => {
+
+    modules.forEach(function(mod) {
+      if (!mod.loaded) {
         mod.onLoad.push(whenDepsLoaded);
-
-        function whenDepsLoaded() {
-          if(!mod.loaded)
-            return;
-
-          var args = mod.exports;
-          var exports = moduleFunction.apply(null, args);
-
-          if (myMod) {
-            myMod.exports = exports;
-            myMod.loaded = true;
-            myMod.onLoad.forEach(function(f) { f(); });
-          }
-        }
-        whenDepsLoaded();
-
-    }, function(error) {
-      fail(new Error(error));
+      }
     });
-  });
+
+    function whenDepsLoaded() {
+      if (!modules.every(function(m) { return m.loaded; }))
+        return;
+
+      var args = modules.map(function(m) {
+        return m.exports;
+      });
+
+      var exports = moduleFunction.apply(null, args);
+      if (myMod) {
+        myMod.exports = exports;
+        myMod.loaded = true;
+        myMod.onLoad.forEach(function(f) { f(); });
+      }
+    }
+
+    whenDepsLoaded();
+  }).catch((error) => console.log("Error: " + error));
 };
